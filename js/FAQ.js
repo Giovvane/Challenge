4d1a949d1a4a0fc37f5453a71fea1093bd3ef11d
AlvaroHolanda <alvaromoraisholanda@gmail.com>
2026-05-23 11:44:09 -0300

    feat: adiciona envio do formulário FAQ com mensagem de sucesso

--- js/FAQ.js
+++ js/FAQ.js
@@ -8,4 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
       itemPai.classList.toggle("ativo");
     });
   });
+    
+  const formularioFaq = document.getElementById("faq-form");
+  const alertaSucesso = document.getElementById("mensagem-sucesso");
+
+  if (formularioFaq) {
+    formularioFaq.addEventListener("submit", (evento) => {
+      evento.preventDefault(); 
+      alertaSucesso.style.display = "block";
+      formularioFaq.reset();
+
+      setTimeout(() => {
+        alertaSucesso.style.display = "none";
+      }, 5000);
+    });
+  }
 });
